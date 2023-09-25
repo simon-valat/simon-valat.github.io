be72f450ffe8c6b005fd7c15d368c9b6ef7abef9
simon valat <simon.valat@finzzle-groupe.com>
2023-09-25 18:33:11 +0200

    Prime theme and base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/primeng/shared.module';
+import { BaseModule } from './base/base.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    BaseModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
